c86bb528464e49c479a31fbbf4a6e16a305b5647
saraschroeder <sara.schroeder00@gmail.com>
2023-03-06 17:37:37 -0600

    correction to paths in server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./Develop/routes/apiRoutes')(app);
+require('./Develop/routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`Server available at localhost${PORT}`);
