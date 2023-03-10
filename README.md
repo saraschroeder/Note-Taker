1dbce8ceb260a88b796e2277933e8f35d81a5c36
saraschroeder <sara.schroeder00@gmail.com>
2023-03-09 19:33:11 -0600

    added comments in readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Note-Taker
+
+## Description
+
+
+
+[Link to deployed application]()
+
+## Installation
+
+N/A
+
+## Usage
+
+
+
+![alt text](./Develop/public/assets/images/Screenshot1.png)
+
+
+
+## Credits
+
+N/A
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ var uniqid = require('uniqid');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../db/db.json'));
+        res.sendFile(path.join(__dirname, '../develop/db/db.json'));
     });
 
     app.post('/api/notes', (req, res) => {
